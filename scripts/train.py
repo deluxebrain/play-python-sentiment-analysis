762f2121fcd24cc229ae54f401337112b7b28aef
deluxebrain <matt@deluxebrain.com>
2016-06-08 17:23:58 +0100

    Tidied all script

--- scripts/train.py
+++ scripts/train.py
@@ -1,16 +1,9 @@
-from subprocess import check_output, CalledProcessError
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-from sklearn.grid_search import GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.linear_model import SGDClassifier
 import pyprind
-import pandas as pd
 import os
-# import tempfile
 import re
 import numpy as np
 import nltk
@@ -19,21 +12,6 @@ import pickle
 porter = PorterStemmer()
 stop = stopwords.words('english')
 work_path = os.path.join(os.path.expanduser('~'), 'tmp/datasets')
-
-
-def preprocessor(text):
-        # remove html
-        text = re.sub('<[^>]*>', '', text)
-
-        # save emoticons
-        emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-
-        # remove non-word characters and put emoticons back
-        text = re.sub('[\W]+',
-                      ' ',
-                      text.lower()) + ' '.join(emoticons).replace('-', '')
-
-        return text
 
 
 def tokenizer(text):
@@ -77,123 +55,7 @@ def get_minibatch(doc_stream, size):
     return docs, y
 
 
-def aggregate_datasets():
-    progress_bar = pyprind.ProgBar(50000)
-    labels = {'pos': 1, 'neg': 0}
-
-    dataframe = pd.DataFrame()
-
-    # aggregate datasets to single csv
-    for dataset in ('test', 'train'):
-        for sentiment in ('pos', 'neg'):
-
-            subpath = 'datasets/aclImdb/%s/%s' % (dataset, sentiment)
-            path = os.path.join(git_root(), subpath)
-            for file in os.listdir(path):
-                with open(os.path.join(path, file),
-                          'r', encoding='utf-8') as infile:
-                    txt = infile.read()
-                dataframe = dataframe.append([[txt, labels[sentiment]]],
-                                             ignore_index=True)
-                progress_bar.update()
-
-    dataframe.columns = ['review', 'sentiment']
-
-    return dataframe
-
-
-def randomize_dataframe(dataframe):
-    np.random.seed(0)
-    dataframe = dataframe.reindex(np.random.permutation(dataframe.index))
-    return dataframe
-
-
-def save_dataframe(dataframe):
-    # home_dir = os.path.join(os.path.expanduser('~'), 'tmp')
-    # temp_dir = tempfile.mkdtemp(dir=home_dir)
-    dataframe_path = os.path.join(work_path, 'movie_data.csv')
-    print("Saving dataframe to {0}".format(dataframe_path))
-    dataframe.to_csv(dataframe_path, index=False)
-
-    return dataframe_path
-
-
-def load_dataframe(path):
-    dataframe = pd.read_csv(path, index=False)
-    return dataframe
-
-
-def train(dataframe):
-    x_train = dataframe.loc[:25000, 'review'].values
-    y_train = dataframe.loc[:25000, 'sentiment'].values
-    x_test = dataframe.loc[25000:, 'review'].values
-    y_test = dataframe.loc[25000:, 'sentiment'].values
-
-    tfidf = TfidfVectorizer(strip_accents=None,
-                            lowercase=False,
-                            preprocessor=None)
-
-    param_grid = [{'vect__ngram_range': [(1, 1)],
-                   'vect__stop_words': [stop, None],
-                   'vect__tokenizer': [tokenizer_simple, tokenizer_porter],
-                   'clf__penalty': ['l1', 'l2'],
-                   'clf__C': [1.0, 10.0, 100.0]},
-                  {'vect__ngram_range': [(1, 1)],
-                   'vect__stop_words': [stop, None],
-                   'vect__tokenizer': [tokenizer_simple, tokenizer_porter],
-                   'vect__use_idf':[False],
-                   'vect__norm':[None],
-                   'clf__penalty': ['l1', 'l2'],
-                   'clf__C': [1.0, 10.0, 100.0]},
-                  ]
-
-    lr_tfidf = Pipeline([('vect', tfidf),
-                         ('clf', LogisticRegression(random_state=0))])
-
-    gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
-                               scoring='accuracy',
-                               cv=5,
-                               verbose=1,
-                               n_jobs=-1)
-
-    gs_lr_tfidf.fit(x_train, y_train)
-
-    print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
-    print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
-
-    clf = gs_lr_tfidf.best_estimator_
-    print('Test Accuracy: %.3f' % clf.score(x_test, y_test))
-
-
-def dump(clf):
-        dest = os.path.join(work_path, 'pkl_objects')
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-
-        pickle.dump(stop,
-                    open(os.path.join(dest, 'stopwords.pkl'), 'wb'),
-                    protocol=4)
-        pickle.dump(clf,
-                    open(os.path.join(dest, 'classifier.pkl'), 'wb'),
-                    protocol=4)
-
-
-def main():
-
-    nltk.download('stopwords')
-
-    # df = aggregate_datasets()
-    # df = randomize_dataframe(df)
-
-    # preprocess each review in the dataset
-    # df['review'] = df['review'].apply(preprocessor)
-
-    # save_dataframe(df)
-
-    # train(df)
-
-    #
-    # equivalent to above but using streamed dataset
+def train():
     vect = HashingVectorizer(decode_error='ignore',
                              n_features=2**21,
                              preprocessor=None,
@@ -219,6 +81,27 @@ def main():
 
     clf = clf.partial_fit(X_test, y_test)
 
-    dump(clf)
+    return clf
+
+
+def dump(object, name):
+        dest = os.path.join(work_path, 'pkl_objects')
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+
+        file_name = name + '.pkl'
+        pickle.dump(object,
+                    open(os.path.join(dest, file_name), 'wb'),
+                    protocol=4)
+
+
+def main():
+
+    nltk.download('stopwords')
+
+    clf = train()
+
+    dump(clf, 'classifier')
+    dump(stop, 'stopwords')
 
 main()
